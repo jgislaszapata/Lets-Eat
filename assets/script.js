127ef7b5bbb2b346ccd95e22a6224843099eea30
Jesus Islas <jesusislas96@gmail.com>
2022-06-13 19:22:57 -0400

    created variables for input boxes, result button, and price input buttons.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+var locationInput = document.getElementById('location-input');
+var priceLow = document.getElementById('price-low');
+var priceMid = document.getElementById('price-mid');
+var priceHigh = document.getElementById('price-high');
+var priceMax = document.getElementById('price-max');
+var generateResults = document.getElementById('search-button');
+var resultsDiv = document.getElementById('results-area');
+
 let apiKey = "sCOUHKS8zI6ZSmRRCMO3iPoVgWunRDXXHBEa6TaOSJqOAcxtj73t4_QlHGSfDXnFFGz38wgbmzC-DydKuW10RR4Fk6vAUY9zEKBf36fpG_q-UaioUK8y8rCUKSuhYnYx";
 let queryURL = "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972";
 async function callAPI (){
