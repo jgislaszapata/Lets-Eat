ab12dd7f2fabd02990836a54a7da75db7c803243
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-06-13 20:54:35 -0400

    test

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,24 @@
-let apiKey = "sCOUHKS8zI6ZSmRRCMO3iPoVgWunRDXXHBEa6TaOSJqOAcxtj73t4_QlHGSfDXnFFGz38wgbmzC-DydKuW10RR4Fk6vAUY9zEKBf36fpG_q-UaioUK8y8rCUKSuhYnYx";
-let queryURL = "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972";
-async function callAPI (){
-  console.log("TEST!!!")
-  const options = {
-    headers:{
-    contentType: "application/json",
-    Authorization: `Bearer ${apiKey}`
-    },
-    mode: "no-cors"
-  };
-  const res = await fetch(queryURL,options);
-  await res.json();
-  console.log(res);
+$("#searchBtn").click(function(findResturants) {
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("language", "en_US");
+encodedParams.append("limit", "30");
+encodedParams.append("location_id", "297704");
+encodedParams.append("currency", "USD");
+
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': 'dbb930ff0emsh649dcb8a3228950p19ae08jsn5b7d964da6d1',
+		'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
+	},
+	body: encodedParams
 };
-callAPI();
+
+fetch('https://worldwide-restaurants.p.rapidapi.com/search', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+});
