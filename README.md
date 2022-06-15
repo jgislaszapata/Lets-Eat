19d1420f7db969989a7880b59ff3a67a521fd4c9
VT_AOE_91 <amartin80108@gmail.com>
2022-06-14 21:02:44 -0400

    Formatting fixes. Added comments. Filled out README. Added screenshots. Fixed search bar width.

--- README.md
+++ README.md
@@ -1,15 +1,45 @@
 # Lets-Eat
-First group assignment for GT-Bootcamp 
+Project #1 for GT Coding Bootcamp 2022
+
+PROJECT TEAM MEMBERS
+---
+Jesus Islas
+
+Ranjani Siva
+
+Oluwaseyi M Oshinowo
+
+Aldo Pelayo
+
+Adam Martin
 
 PROJECT DESCRIPTION
+---
+The user will search an ingredient in a text field. The application will then return 6 results with the following information:
 
-Users will be able to answer a questionare based on prefrences of where they would like to eat. This application will pull up restaurant in the area after the user answers said questions that match the users prefrences.
+Dish title
 
-//User story//
+Dish prep time
+
+Dish instructions
+
+Image of dish
+
+USER STORY
+---
 AS A hungry user
-I WANT to get restaurant suggestions based on my current prefrences 
-SO THAT i can decide what would be the best place to eat at
+I WANT to get recipe suggestions based on a chosen ingredient
+SO THAT I have ideas of what I can make, how long it will take to prepare, how to make it, and what it looks like
 
-API's being used:
-Yelp Fusion API
-Spoontacular
+APIs IN THIS APPLICATION
+---
+Spoontacular recipe API
+
+PICTURES OF APPLICATION
+---
+![Image of app with no data entry](assets/SS1.png "Initial App Layout")
+
+![Image of app after searching "sirloin"](assets/SS2.png "Sirloin Search Top Result")
+
+![Image of several returned results](assets/SS3.png "Sirloin Search Additional Results")
+
--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,89 @@
-//get search button element ID
+// Initializes variable for search button results
 var generateResults = document.getElementById('search-button');
  
+// API key for Spoontacular food recipe API
 var apiKey = "4e9c1647bamsh75f9980fca9fd27p176220jsn6936ac7b1a28"
-//global variable to get ingredient ID
+
+// Global variable to get ingredient ID
 var ingredientIDs = [];
 
-//Event listener is added to search button
+// Event listener is added to search button
 $("#results").hide();
 
+// Sets generateResults element to the results of the get recipe function when search-button is clicked
 generateResults.addEventListener("click", getRecipe);
 
-//Function getRecipe is invoked when search button is clicked.
-//This function inturn calls getrecipeID to get top 5 ingredient ID's for the entered ingredient
-//Ingredient IDs are then passed to getRecipe function to retrive information like Title, Image, PrepTime and Instructions 
+// EXPLAINATION OF PRIMARY SCRIPT FUNCTION
+// ************************************************************************************************************************
+// 1) Function getRecipe() is invoked when search button is clicked
+// 2) This function inturn calls getrecipeID() to get the top 5 ingredient ID's for the entered ingredient
+// 3) Ingredient IDs are then passed to getRecipe() function to retrive information like Title, Image, PrepTime and Instructions
+// ************************************************************************************************************************
 
 // The reason behind two API calls is that API used in getRecipe function requires 
 async function getRecipe() {
 
+await getReceipeID(); // Halt function execution until API results are fully returned
+  
+console.log(ingredientIDs); // Print incredient ID to the browser console for verification
 
-await getReceipeID();
- console.log(ingredientIDs);
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': apiKey,
-      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-    }
-  };
-  $("#results").show();
-  for (var i = 0; i < 5; i++) {
-   await fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/' + ingredientIDs[i] + '/information', options)
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data1) {
-        console.log("recipe details" ,data1);
-        $("#title_" + i).text(data1.title);
-        var img = document.createElement("IMG");
-        img.src = data1.image;
-        $("#recipe_" + i).append(img);
-
-        $("#prepTime_" + i).text(data1.readyInMinutes);
-        $("#instruction_" + i).text(data1.instructions);
-
-      })
-     
-      .catch(err => console.error(err));
-
+// APU pull method ("GET") & header variables ("apiKey", API host URL)
+const options = {
+  method: 'GET', // API command to retrieve results
+  headers: {
+    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
   }
-}
+};
+
+// Set results ID elements in the HTML and displays
+$("#results").show();
+  for (var i = 0; i < 5; i++) { // Loop returning 6 total results from the search "i,0,6"
+  await fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/' + ingredientIDs[i] + '/information', options)
+  .then(function (res) {
+    return res.json(); // Return API results as a JSON object
+  })
+  .then(function (data1) {
+    console.log("recipe details" ,data1); // Log receipe results to browser console for dev validation
+    $("#title_" + i).text(data1.title); // Applies title value from API to the coorresponding (i) HTML element
+    var img = document.createElement("IMG"); // Initialize an image element for the HTML
+    img.src = data1.image; // Image source 
+    $("#recipe_" + i).append(img); // Apply recipe from API to corresponding (i) HTML element
+    $("#prepTime_" + i).text(data1.readyInMinutes); // Apply prep time from API to the corresponding (i) HTML element
+    $("#instruction_" + i).text(data1.instructions); // Apply instructions from API to the corresponding (i) HTML element
+  })
+  .catch(err => console.error(err)); // Error catch from API call to console log an error
+  }
+
+} // End getRecipe() function
 
 //This API call converts the user entered ingredient and fetchs corresponding ID
 async function getReceipeID() {
+  //API query values
   const options = {
-    method: 'GET',
+    method: 'GET', // GET method for API to return results
     headers: {
       'X-RapidAPI-Key': apiKey,
       'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
     }
   };
+  
+  // Initialize variable based on user text input value requesting ingredient name
   var ingredientSearch = $("#location-input").val();
- await fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=' + ingredientSearch, options)
+    await fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=' + ingredientSearch, options)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log("receipe id" ,data);
-       ingredientIDs = [];
+      console.log("receipe id" ,data); // Console log recipe ID and corresponding data to browser console
+      ingredientIDs = []; // Empty array to store ingredient IDs
+      // Loop retrieving 6 ingredient IDs i,0,6
       for (var i = 0; i < 5; i++) {
         ingredientIDs[i] = data[i].id;
       }
-      console.log("ingredient id", ingredientIDs);
-    
+      console.log("ingredient id", ingredientIDs); // Print ingredient ID's to browser console
     })
-    .catch(err => console.error(err));
-}
+    .catch(err => console.error(err)); // API call error handling
+
+} // End of the getRecipeID() function
 
